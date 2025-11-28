2658e2a887645410606fb4cddc90d9b1a96b43de
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-11-28 18:25:42 +0430

    calculator test setup

---
+++ TypeScript-Node-Testing/2-jest/src/__tests__/Calculator.spec.ts
@@ -0,0 +1,12 @@
+import Calculator from "../Calculator";
+
+describe("Calculator", () => {
+  let calculator: Calculator;
+  beforeEach(() => {
+    calculator = new Calculator();
+  });
+
+  describe("add method", () => {
+    it("should add two numbers correctly", () => {});
+  });
+});
